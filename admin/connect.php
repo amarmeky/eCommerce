8cf019a52345bcb3ffba12631b4fc39e8d9a787c
Amar Yasser Meky <amar.meky@almotahida.local>
2025-04-20 13:35:57 +0200

    add data base

---
+++ admin/connect.php
@@ -0,0 +1,16 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=shop';
+$username = 'root';
+$password = '';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_PERSISTENT => false,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+try {
+    $conn = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+    
