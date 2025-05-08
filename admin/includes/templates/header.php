09b09fb8ac579c6068142c5c634f658ea188c798
Amar Meky <amarmeky7@gmail.com>
2025-05-08 14:15:15 +0300

    edit function file

--- admin/includes/templates/header.php
+++ admin/includes/templates/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle?></title> 
+    <title><?php  gettitle();?></title> 
     <link rel="stylesheet" href="<?php echo $css?>bootstrap.min.css"/>
     <link rel="stylesheet" href="<?php echo $css?>fontawesome.min.css"/>
     <link rel="stylesheet" href="<?php echo $css?>all.min.css"/>
